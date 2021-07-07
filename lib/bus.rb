e2778c6fdf6beb6d4a25ffa49dbcdde0dd72698d
Sawaez Amin <sawaezamin@gmail.com>
2021-07-07 15:19:21 -0600

    Initial commit

---
+++ lib/bus.rb
@@ -0,0 +1,36 @@
+class Bus
+  attr_reader :name, :capacity, :passengers
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @passengers = []
+  end
+  # everything above this is iteration 1
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  def yell_at_passengers
+    new_passengers = []
+    @passengers.each do |name|
+      new_passengers << name.upcase
+    end
+    new_passengers
+  end
+  # the two methods above is iteration 2
+
+  def number_of_passengers
+    @passengers.length
+  end
+
+  def over_capacity?
+    return false if @passengers.length <= @capacity
+    true
+  end
+  #the two methods above is iteration 3
+
+  def kick_out
+    @passengers.shift
+  end
+end
--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -3,25 +3,25 @@ require './lib/bus'
 
 describe Bus do
   context 'Attributes' do
-    xit 'is a bus' do
+    it 'is a bus' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus).to be_an_instance_of(Bus)
     end
 
-    xit 'has a name' do
+    it 'has a name' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.name).to eq("Mikes Awesome Bus")
     end
 
-    xit 'has a capacity' do
+    it 'has a capacity' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.capacity).to eq(4)
     end
 
-    xit 'has no passengers by default' do
+    it 'has no passengers by default' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.passengers).to eq([])
@@ -30,7 +30,7 @@ describe Bus do
 
   # Iteration 2
   context 'Passengers' do
-    xit 'returns a list of passengers' do
+    it 'returns a list of passengers' do
       bus = Bus.new('Mikes Awesome Bus', 4)
       bus.add_passenger('Mike')
       bus.add_passenger('Megan')
@@ -39,13 +39,61 @@ describe Bus do
       expect(bus.passengers).to eq(['Mike', 'Megan', 'Tim'])
     end
 
-    xit 'returns a list of uppercased names' do
+    it 'returns a list of uppercased names' do
       bus = Bus.new('Mikes Awesome Bus', 4)
       bus.add_passenger('Mike')
       bus.add_passenger('Megan')
       bus.add_passenger('Tim')
 
       expect(bus.yell_at_passengers).to eq(['MIKE', 'MEGAN', 'TIM'])
+    end
+
+    #blocks below is iteration 3
+    it 'returns current # of passengers' do
+      bus = Bus.new('Mikes Awesome Bus', 4)
+      bus.add_passenger('Mike')
+      bus.add_passenger('Megan')
+      bus.add_passenger('Tim')
+
+      expect(bus.number_of_passengers).to eq(3)
+    end
+
+    it 'returns if bus is at capacity' do
+      bus = Bus.new('Mikes Awesome Bus', 4)
+      bus.add_passenger('Mike')
+      bus.add_passenger('Megan')
+      bus.add_passenger('Tim')
+
+      expect(bus.over_capacity?).to eq(false)
+
+      bus.add_passenger('Eve')
+      bus.add_passenger('Alice')
+
+      expect(bus.number_of_passengers).to eq(5)
+      expect(bus.over_capacity?).to eq(true)
+    end
+
+    #block below is iteration 4
+    it 'kicks out passengers if bus is at capacity' do
+      bus = Bus.new('Mikes Awesome Bus', 4)
+      bus.add_passenger('Mike')
+      bus.add_passenger('Megan')
+      bus.add_passenger('Tim')
+      bus.add_passenger('James')
+      bus.add_passenger('Cat')
+      bus.add_passenger('Alice')
+
+      bus.kick_out
+      expect(bus.number_of_passengers).to eq(5)
+      expect(bus.over_capacity?).to eq(true)
+
+      2.times do
+        bus.kick_out
+      end
+      
+      expect(bus.number_of_passengers).to eq(3)
+      expect(bus.over_capacity?).to eq(false)
+      expect(bus.passengers).to eq(['James', 'Cat', 'Alice'])
     end
   end
 end
